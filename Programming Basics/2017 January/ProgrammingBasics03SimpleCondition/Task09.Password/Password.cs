69a1261bd26a53f957310e9167776c2a968da7c9
andreykata <andreykata85@yahoo.com>
2017-02-11 10:51:17 +0200

    add to tasks

---
+++ Programming Basics/2017 January/ProgrammingBasics03SimpleCondition/Task09.Password/Password.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Password
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+
+        if (input == "s3cr3t!P@ssw0rd")
+        {
+            Console.WriteLine("Welcome");
+        }
+        else
+        {
+            Console.WriteLine("Wrong password!");
+        }
+    }
+}
---
+++ Programming Basics/2017 January/ProgrammingBasics03SimpleCondition/Task11.EqualWords/EqualWords.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class EqualWords
+{
+    static void Main(string[] args)
+    {
+        var firstWord = Console.ReadLine().ToUpper();
+        var secondWord = Console.ReadLine().ToUpper();
+
+        firstWord.ToLower();
+        secondWord.ToLower();       
+        
+        if (firstWord.Equals(secondWord))
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+
+        Console.WriteLine(firstWord);
+        // Console.WriteLine(firstWord.Length);    
+    }
+}
