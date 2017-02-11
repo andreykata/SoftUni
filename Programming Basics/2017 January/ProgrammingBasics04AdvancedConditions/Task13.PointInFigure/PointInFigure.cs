782587502cd9fcc3aadeb81fa3aea5b911dfacf2
andreykata <andreykata85@yahoo.com>
2017-02-11 14:21:47 +0200

    add last tasks

---
+++ Programming Basics/2017 January/ProgrammingBasics04AdvancedConditions/Task13.PointInFigure/PointInFigure.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class PointInFigure
+{
+    static void Main(string[] args)
+    {
+        var h = int.Parse(Console.ReadLine());
+        var x = int.Parse(Console.ReadLine());
+        var y = int.Parse(Console.ReadLine());
+
+        var firstFigure = x > 0 && x < (3 * h) && y > 0 && y < h;
+        var secondFigure = x > h && x < (2 * h) && y > h && y < (4 * h);
+
+        if (firstFigure || secondFigure || (x > h && x < (2 * h) && y == h))
+        {
+            Console.WriteLine("inside");
+        }
+        else if ((x < 0 || x > (3 * h) || y < 0 || y > h) &&
+            (x < h || x > (2 * h) || y < h || y > (4 * h)))
+        {
+            Console.WriteLine("outside");
+        }
+        else
+        {
+            Console.WriteLine("border");
+        }
+    
+    }
+}
