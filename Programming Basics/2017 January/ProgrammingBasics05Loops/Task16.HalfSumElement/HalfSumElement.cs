0133aba8402c3c4ded2a83432a0ad5420b581299
andreykata <andreykata85@yahoo.com>
2017-02-12 16:56:22 +0200

    add task16.HalfSumElement

---
+++ Programming Basics/2017 January/ProgrammingBasics05Loops/Task16.HalfSumElement/HalfSumElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class HalfSumElement
+{
+    static void Main(string[] args)
+    {
+        var numbers = int.Parse(Console.ReadLine());
+        int[] arr = new int[numbers];
+        bool hasAnswer = false;
+
+        for (int i = 0; i < numbers; i++)
+        {
+            var num = int.Parse(Console.ReadLine());
+            arr[i] = num;
+        }
+
+        var sum = arr.Sum();
+
+        for (int i = 0; i < numbers; i++)
+        {
+            var number = arr[i];
+            var currentSum = sum - number;
+
+            if (number == currentSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", currentSum);
+                hasAnswer = true;
+            }            
+        }
+
+        if (hasAnswer == false)
+        {
+            var maxNumber = arr.Max();
+            var currentSum = arr.Sum() - maxNumber;
+            Console.WriteLine("No");
+            Console.WriteLine("Diff = {0}", Math.Abs(maxNumber - currentSum));
+        }
+    }
+}
