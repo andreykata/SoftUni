ae5dc55608accfeef796bccdb70d72b98a98c82c
andreykata <andreykata85@yahoo.com>
2017-02-17 20:27:47 +0200

    add two last draw figures tasks

---
+++ Programming Basics/2017 January/ProgrammingBasics06DrawFigures/Task09.House/House.cs
@@ -0,0 +1,58 @@
+﻿using System;
+class House
+{
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        int roofRows = 0;
+        int x = n / 2 - 1;
+        int y = n / 2;
+
+        if (n % 2 == 0)
+        {
+            roofRows = n / 2;
+        }
+        else
+        {
+            roofRows = n / 2 + 1;
+            x = y;
+        }
+
+        // Draw roof house
+        for (int row = 0; row < roofRows; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (col >= x && col <= y)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("-");
+                }
+            }
+            x--;
+            y++;
+            Console.WriteLine();
+        }
+
+        // Draw body house
+        for (int row = 0; row < n / 2; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (col == 0 || col == n - 1)
+                {
+                    Console.Write("|");
+                }
+                else
+                {
+                    Console.Write("*");
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Programming Basics/2017 January/ProgrammingBasics06DrawFigures/Task10.Diamant/Diamant.cs
@@ -0,0 +1,58 @@
+﻿using System;
+class Diamant
+{
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        var dimantRows = n - 1;
+        var x = n / 2 - 1;
+        var y = n / 2;
+
+        if (n % 2 == 1)
+        {
+            dimantRows = n;
+            x = y;
+        }
+
+        // Draw first half
+        for (int row = 0; row < dimantRows / 2 + 1; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (col == x || col == y)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("-");
+                }
+            }
+            x--;
+            y++;
+            Console.WriteLine();
+        }
+
+
+        // Draw second half
+        x = 1;
+        y = n - 2;
+        for (int row = 0; row < dimantRows / 2; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (col == x || col == y)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("-");
+                }
+            }
+            x++;
+            y--;
+            Console.WriteLine();
+        }
+    }
+}
